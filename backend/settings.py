a50ce54f28eeb9ef34f1791ba2b9f80c8873857c
Mumin Bhat <mumin.fayaz5050@gmail.com>
2023-09-14 18:44:43 +0530

    postgress database

--- backend/settings.py
+++ backend/settings.py
@@ -93,13 +93,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': dj_database_url.config(default=('postgresql://postgres:mqT5ihz1HUkDyOzekqGi@containers-us-west-176.railway.app:7828/railway'))
     }
-}
 
 
 # Password validation
@@ -212,3 +210,8 @@ from corsheaders.defaults import default_headers
 CORS_ALLOW_HEADERS = default_headers + (
     'Access-Control-Allow-Origin',
 )
+
+
+# Hosting
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
